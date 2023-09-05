babeddbc76c2002aa1df137794d8009fcfb91d16
Borui He <58924223+BoruiHe@users.noreply.github.com>
2023-09-05 10:02:33 -0400

    Add files via upload

--- utils/visualization_training_gaga.py
+++ utils/visualization_training_gaga.py
@@ -55,11 +55,11 @@ def vis_training_m2(checkpoint_folder_name):
 
         path = os.path.join(rp, 'checkpoints', checkpoint_folder_name, str(seed))
 
-        # checkpoint_path = os.path.join(path, 'm1_last.pth')
-        # checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+        checkpoint_path = os.path.join(path, 'm1_last.pth')
+        checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
 
-        # axs1['Training'].plot(x, checkpoint['train loss'], label=str(seed))
-        # axs1['Validation'].plot(x, checkpoint['val loss'], label=str(seed))
+        axs1['Training'].plot(x, checkpoint['train loss'], label=str(seed))
+        axs1['Validation'].plot(x, checkpoint['val loss'], label=str(seed))
 
         checkpoint_path = os.path.join(path, 'm2.pkl')
         checkpoint = pk.load(open(checkpoint_path, 'rb'))
@@ -67,17 +67,17 @@ def vis_training_m2(checkpoint_folder_name):
         axs2['Training'].scatter(HpParams['random seeds'].index(seed), checkpoint['train acc'], label=str(seed))
         axs2['Validation'].scatter(HpParams['random seeds'].index(seed), checkpoint['val acc'], label=str(seed))
 
-    # axs1['Training'].set_title('Training loss')
-    # axs1['Training'].set_xlabel('epoch')
-    # axs1['Training'].set_ylabel('loss')
-    # axs1['Training'].legend()
-    # axs1['Validation'].set_title('Validation loss')
-    # axs1['Validation'].set_xlabel('epoch')
-    # axs1['Validation'].set_ylabel('loss')
-    # axs1['Validation'].legend()
+    axs1['Training'].set_title('Training loss')
+    axs1['Training'].set_xlabel('epoch')
+    axs1['Training'].set_ylabel('loss')
+    axs1['Training'].legend()
+    axs1['Validation'].set_title('Validation loss')
+    axs1['Validation'].set_xlabel('epoch')
+    axs1['Validation'].set_ylabel('loss')
+    axs1['Validation'].legend()
 
     save_path = os.path.join(rp, 'checkpoints', checkpoint_folder_name)
-    # fig1.savefig(os.path.join(save_path, f'train&val loss m2.png'))
+    fig1.savefig(os.path.join(save_path, f'train&val loss m2.png'))
 
     axs2['Training'].set_title('Training accuracy')
     axs2['Training'].set_xlabel('random seeds')
